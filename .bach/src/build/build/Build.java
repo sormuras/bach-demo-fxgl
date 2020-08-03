4fd3bbad991f084ab26f08202521393d1b60093c
Christian Stein <sormuras@gmail.com>
2020-08-03 07:11:27 +0200

    Initial commit

---
+++ .bach/src/build/build/Build.java
@@ -0,0 +1,54 @@
+package build;
+
+import de.sormuras.bach.Bach;
+import de.sormuras.bach.Configuration;
+import de.sormuras.bach.Project;
+import de.sormuras.bach.project.Feature;
+import de.sormuras.bach.project.Link;
+import java.util.Locale;
+
+class Build {
+  public static void main(String... args) {
+    var configuration = Configuration.ofSystem();
+    var project =
+        Project.ofCurrentDirectory()
+            .without(Feature.CREATE_API_DOCUMENTATION)
+            .without(Feature.CREATE_CUSTOM_RUNTIME_IMAGE)
+            .with(
+                Link.of(
+                    "com.fasterxml.jackson.databind",
+                    "https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.11.2/jackson-databind-2.11.2.jar"))
+            .with(
+                Link.of(
+                    "com.fasterxml.jackson.annotation",
+                    "https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.11.2/jackson-annotations-2.11.2.jar"))
+            .with(
+                Link.of(
+                    "kotlin.stdlib",
+                    "https://repo.maven.apache.org/maven2/org/jetbrains/kotlin/kotlin-stdlib/1.3.72/kotlin-stdlib-1.3.72.jar"))
+            .with(linkModuleOfJavaFX("base"))
+            .with(linkModuleOfJavaFX("controls"))
+            .with(linkModuleOfJavaFX("fxml"))
+            .with(linkModuleOfJavaFX("graphics"))
+            .with(linkModuleOfJavaFX("media"))
+            .with(linkModuleOfJavaFX("swing"));
+
+    new Bach(configuration, project).build();
+  }
+
+  // https://repo.maven.apache.org/maven2/org/openjfx
+  static Link linkModuleOfJavaFX(String suffix) {
+    var group = "org.openjfx";
+    var version = "14.0.2.1";
+    var platform = of("linux", "mac", "win");
+    var coordinates = String.join(":", group, "javafx-" + suffix, version, platform);
+    return Link.ofCentral("javafx." + suffix, coordinates);
+  }
+
+  static <T> T of(T linux, T mac, T windows) {
+    var os = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
+    if (os.contains("win")) return windows;
+    if (os.contains("mac")) return mac;
+    return linux;
+  }
+}
---
+++ com.github.sormuras.bach.fxgl/module-info.java
@@ -0,0 +1,6 @@
+module com.github.sormuras.bach.fxgl {
+  requires com.almasb.fxgl.all;
+
+  exports com.github.sormuras.bach.fxgl to
+      com.almasb.fxgl.core;
+}
