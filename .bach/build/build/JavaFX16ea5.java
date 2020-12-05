a7b3bb4268aac7a76777172506d4bf27f132e3fe
Christian Stein <sormuras@gmail.com>
2020-12-05 22:25:23 +0100

    Upgrade to JavaFX 16-ea+5 and Bach 16-ea

---
+++ .bach/build/build/JavaFX16ea5.java
@@ -0,0 +1,9 @@
+package build;
+
+import com.github.sormuras.bach.project.ModuleLookup;
+
+public class JavaFX16ea5 extends ModuleLookup.JavaFX {
+  public JavaFX16ea5() {
+    super("16-ea+5");
+  }
+}
---
+++ .bach/build/module-info.java
@@ -0,0 +1,28 @@
+import com.github.sormuras.bach.project.Feature;
+import com.github.sormuras.bach.project.ProjectInfo;
+import com.github.sormuras.bach.project.ProjectInfo.ExternalModules;
+import com.github.sormuras.bach.project.ProjectInfo.ExternalModules.Link;
+
+@ProjectInfo(
+    name = "bach-fxgl",
+    features = Feature.GENERATE_CUSTOM_RUNTIME_IMAGE,
+    externalModules =
+        @ExternalModules(
+            links = {
+              @Link(
+                  module = "com.fasterxml.jackson.annotation",
+                  to =
+                      "https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.11.2/jackson-annotations-2.11.2.jar"),
+              @Link(
+                  module = "com.fasterxml.jackson.databind",
+                  to =
+                      "https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.11.2/jackson-databind-2.11.2.jar"),
+            },
+            lookups = build.JavaFX16ea5.class
+        ),
+    tweaks = @ProjectInfo.Tweak(tool = "jlink", args = {"--launcher", "bach-fxgl=com.github.sormuras.bach.fxgl"})
+)
+open module build {
+  requires com.github.sormuras.bach;
+  // TODO provides com.github.sormuras.bach.project.ModuleLookup with build.JavaFX16ea5;
+}
