a826bb734b5276567ae9b49728c812c1942504d6
Christian Stein <sormuras@gmail.com>
2020-12-05 23:00:51 +0100

    Fix https-related handshake failure

--- com.github.sormuras.bach.fxgl/module-info.java
+++ com.github.sormuras.bach.fxgl/module-info.java
@@ -1,5 +1,6 @@
 module com.github.sormuras.bach.fxgl {
   requires com.almasb.fxgl.all;
+  requires jdk.crypto.ec; // https://stackoverflow.com/questions/55439599
 
   exports com.github.sormuras.bach.fxgl to
       com.almasb.fxgl.core;
