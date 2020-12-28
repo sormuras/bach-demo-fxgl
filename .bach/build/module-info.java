20352b43462a4a03274bd77bd744950c67643476
Christian Stein <sormuras@gmail.com>
2020-12-28 08:00:55 +0100

    Upgrade to Java 17-ea and Bach 16 with bach-build@v1

--- .bach/build/module-info.java
+++ .bach/build/module-info.java
@@ -1,24 +1,20 @@
 import com.github.sormuras.bach.project.Feature;
 import com.github.sormuras.bach.project.ProjectInfo;
-import com.github.sormuras.bach.project.ProjectInfo.ExternalModules;
-import com.github.sormuras.bach.project.ProjectInfo.ExternalModules.Link;
+import com.github.sormuras.bach.project.ProjectInfo.Link;
 
 @ProjectInfo(
     name = "bach-fxgl",
     features = Feature.GENERATE_CUSTOM_RUNTIME_IMAGE,
-    externalModules =
-        @ExternalModules(
-            links = {
-              @Link(
-                  module = "com.fasterxml.jackson.annotation",
-                  to = "https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.11.2/jackson-annotations-2.11.2.jar"),
-              @Link(
-                  module = "com.fasterxml.jackson.databind",
-                  to = "https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.11.2/jackson-databind-2.11.2.jar"),
-            }
-        )
+    links = {
+        @Link(
+            module = "com.fasterxml.jackson.annotation",
+            to = "https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.11.2/jackson-annotations-2.11.2.jar"),
+        @Link(
+            module = "com.fasterxml.jackson.databind",
+            to = "https://repo.maven.apache.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.11.2/jackson-databind-2.11.2.jar"),
+    }
 )
 module build {
   requires com.github.sormuras.bach;
-  provides com.github.sormuras.bach.project.ModuleLookup with build.JavaFX16ea5;
+  provides com.github.sormuras.bach.BuildProgram with build.Program;
 }
