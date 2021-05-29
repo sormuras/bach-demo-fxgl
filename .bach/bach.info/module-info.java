8ecc87c4a59082a779bff6fdf8efe0b80d99a47d
Christian Stein <sormuras@gmail.com>
2021-05-29 12:32:30 +0200

    Upgrade to Bach 17-ea

--- .bach/bach.info/module-info.java
+++ .bach/bach.info/module-info.java
@@ -1,17 +1,26 @@
-import com.github.sormuras.bach.ProjectInfo;
-import com.github.sormuras.bach.ProjectInfo.*;
+import static com.github.sormuras.bach.api.ExternalLibraryName.*;
+
+import com.github.sormuras.bach.api.ProjectInfo;
+import com.github.sormuras.bach.api.ProjectInfo.*;
 
 @ProjectInfo(
     name = "bach-fxgl",
     version = "1-ea",
-    options = @Options(tools = @Tools(skip = "javadoc")),
-    libraries =
-        @Libraries(
-            externalLibraries = {
-              @ExternalLibrary(name = LibraryName.JAVAFX, version = "16"),
-              @ExternalLibrary(name = LibraryName.FXGL, version = "11.15"),
-              @ExternalLibrary(name = LibraryName.SORMURAS_MODULES, version = "2021.04.13")
-            }))
+    external =
+        @External(
+            libraries = {
+              @ExternalLibrary(name = JAVAFX, version = "16"),
+              @ExternalLibrary(name = FXGL, version = "11.15"),
+              @ExternalLibrary(name = SORMURAS_MODULES, version = "2021.04.13")
+            }),
+    tool =
+        @Tool(
+            skip = "javadoc",
+            tweaks =
+                @Tweak(
+                    tool = "jlink",
+                    with = "--launcher",
+                    more = "bach-fxgl=com.github.sormuras.bach.fxgl")))
 module bach.info {
   requires com.github.sormuras.bach;
 }
