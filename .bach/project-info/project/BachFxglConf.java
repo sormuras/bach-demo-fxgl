772c86ab0aa2cb146f3a1ab728904ee960cc99f5
Christian Stein <sormuras@gmail.com>
2022-06-30 07:58:42 +0200

    Use static bills of modules

--- .bach/project-info/project/BachFxglConf.java
+++ .bach/project-info/project/BachFxglConf.java
@@ -8,9 +8,6 @@ public final class BachFxglConf implements Configurator {
     public Project configureProject(Project project) {
         return project
             .with(FXGL.version("17.1"))
-            .with(GluonAttach.version("4.0.14"))
-            .with(Jackson.version("2.13.3"))
-            //.with(JavaFX.version("18.0.1"))
-            .with(Kotlin.version("1.7.0"));
+            .with(GluonAttach.version("4.0.14"));
     }
 }
